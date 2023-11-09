edbd56651eda20a0ec4fec08054c84a0e7e1f723
Дмитрий Литвиненко <dmitryl@Dmitrijs-MacBook-Air.local>
2023-11-09 13:49:01 +0300

    feat: start working with auth forms

--- src/app/login-page/login-page.component.ts
+++ src/app/login-page/login-page.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-login-page',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, RouterLink],
   templateUrl: './login-page.component.html',
   styleUrl: './login-page.component.scss'
 })
--- src/app/shared/layouts/auth-layout/auth-layout.component.ts
+++ src/app/shared/layouts/auth-layout/auth-layout.component.ts
@@ -1,14 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { RouterOutlet } from '@angular/router'
+import { RouterLink, RouterOutlet } from '@angular/router'
 
 @Component({
   selector: 'app-auth-layout',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [CommonModule, RouterOutlet, RouterLink],
   templateUrl: './auth-layout.component.html',
   styleUrl: './auth-layout.component.scss'
 })
 export class AuthLayoutComponent {
+
+
+  
+
 
 }

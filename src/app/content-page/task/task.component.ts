40f9fbd4bbacc8883c4f57d4d510483942faf9bf
Dmitry Litvinenko <ds-sev@ya.ru>
2023-11-13 15:19:07 +0300

    feat: create component for create/edit task

--- src/app/content-page/task/task.component.ts
+++ src/app/content-page/task/task.component.ts
@@ -1,22 +1,28 @@
-import { Component, inject, Input } from '@angular/core'
+import { Component, EventEmitter, inject, Input, Output } from '@angular/core'
 import { CommonModule } from '@angular/common';
 import { ITask } from '../../shared/interfaces'
 import { ButtonModule } from 'primeng/button'
 import { CheckboxModule } from 'primeng/checkbox'
 import { FormsModule } from '@angular/forms'
 import { TasksService } from '../../shared/services/tasks.service'
+import { TaskFormComponent } from '../task-form/task-form.component'
+import { DialogModule } from 'primeng/dialog'
 
 @Component({
   selector: '[task-table-row]',
   standalone: true,
-  imports: [CommonModule, ButtonModule, CheckboxModule, FormsModule],
+  imports: [CommonModule, ButtonModule, CheckboxModule, FormsModule, TaskFormComponent, DialogModule],
   templateUrl: './task.component.html',
   styleUrl: './task.component.scss'
 })
 export class TaskComponent {
   @Input({required: true}) task!: ITask
+  @Output() onEditClick = new EventEmitter()
 
   tasksService = inject(TasksService)
+  editForm: TaskFormComponent
+
+  openModal: boolean = false
 
   // public priority = Priority
 
@@ -29,7 +35,13 @@ export class TaskComponent {
     this.tasksService.deleteTask(this.task.id)
   }
 
+  onEditTaskClick() {
+    // this.editForm.displayModal = true
+    console.log(this.task)
+  }
+
   toggleTaskState() {
     console.log(this.task)
   }
+
 }

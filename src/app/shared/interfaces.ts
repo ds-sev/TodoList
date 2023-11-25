d8508405599e504c172f90a853d92fa8da2abcf1
Dmitry Litvinenko <ds-sev@ya.ru>
2023-11-25 18:21:35 +0300

    fix: change auth method for different users for correct data-processing

--- src/app/shared/interfaces.ts
+++ src/app/shared/interfaces.ts
@@ -1,6 +1,7 @@
 export interface IUser {
   email: string
   password: string
+  data?: ITask[]
 }
 
 export interface ICategory {
--- src/app/shared/services/auth.service.ts
+++ src/app/shared/services/auth.service.ts
@@ -11,29 +11,43 @@ export class AuthService {
   messageService = inject(MessageService)
 
   register(user: IUser) {
-    localStorage.setItem('user', JSON.stringify(user))
+    // localStorage.setItem('user', JSON.stringify(user))
+    const newUserData: IUser = {...user, data: [{id: 'lk', name: 'test'}]}
+    localStorage.setItem(`user_${user.email}`, JSON.stringify(newUserData))
+    // localStorage.setItem(`user_${user.email}`, JSON.stringify(user))
   }
 
   login(user: IUser) {
-    if (localStorage.getItem('user') === JSON.stringify(user) ) {
+
+
+    const userDataString = localStorage.getItem(`user_${user.email}`);
+    const userToAuth: IUser = userDataString ? JSON.parse(userDataString) : null;
+
+    console.log(userToAuth.email, userToAuth.password)
+
+    if (userToAuth.email === user.email && userToAuth.password === user.password) {
+
+
+
+    // if (localStorage.getItem(`user_${user.email}`) === JSON.stringify(user)) {
       localStorage.setItem('authorized', 'yes')
       this.messageService.add({
-        severity:'success',
-        summary:'Ok',
-        detail:'Успешный вход',
+        severity: 'success',
+        summary: 'Ok',
+        detail: 'Успешный вход',
         key: 'notificationToast'
-      });
+      })
     } else {
       this.messageService.add({
-        severity:'error',
-        summary:'Ошибка',
-        detail:'Проверьте правильность ввода логина или пароля',
+        severity: 'error',
+        summary: 'Ошибка',
+        detail: 'Проверьте правильность ввода логина или пароля',
         key: 'notificationToast'
-      });
+      })
     }
   }
 
-  isAuthenticated() : boolean {
+  isAuthenticated(): boolean {
     if (localStorage.getItem('authorized')) {
       return true
     } else {

da2d427d0d7bf57ee5ed0b99f6f3a8ecdee530af
Dmitry Litvinenko <ds-sev@ya.ru>
2023-12-04 16:27:36 +0300

    refactor: change type of view action-buttons in tasks and categories

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,5 @@
 import { NgModule } from '@angular/core'
-import { BrowserModule, HAMMER_GESTURE_CONFIG, HammerModule } from '@angular/platform-browser'
+import { BrowserModule, HammerModule } from '@angular/platform-browser'
 import { AppRoutingModule } from './app-routing.module'
 import { AppComponent } from './app.component'
 import { TableModule } from 'primeng/table'
@@ -8,7 +8,6 @@ import { CheckboxModule } from 'primeng/checkbox'
 import { FormsModule } from '@angular/forms'
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations'
 import { HeaderComponent } from './header/header.component'
-import { MyHammerConfig } from './shared/classes/my-hammer.config'
 import { ToastModule } from 'primeng/toast'
 import { ConfirmationService, MessageService } from 'primeng/api'
 
@@ -29,10 +28,6 @@ import { ConfirmationService, MessageService } from 'primeng/api'
     ToastModule,
   ],
   providers: [
-    {
-      provide: HAMMER_GESTURE_CONFIG,
-      useClass: MyHammerConfig
-    },
     ToastModule,
     ConfirmationService,
     MessageService
